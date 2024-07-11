7dc96c47e81d30d2718fb723b9d197625ac2c151
Hope <hopemwaura39@gmail.com>
2024-07-11 08:57:56 +0300

    fetch with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
+  const inputForm = document.querySelector("form");
+  const input = document.querySelector("input#searchByID");
   
-}
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(e.target.children[1].value)
+    console.log(input.value)
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:5000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
